9736fcec063df86c8e0621a2a9948b8b060bfdea
Masaki Ohsumi <m.osumi@goodpatch.com>
2018-06-18 12:14:36 +0900

    Draw Image of video element when press the take photo button.

--- src/assets/scripts/web-camera.js
+++ src/assets/scripts/web-camera.js
@@ -1,22 +1,36 @@
 class WebCamera {
-  constructor ({$video}) {
+  constructor ({$video, $canvas, $shot}) {
     this.$video = $video
+    this.$canvas = $canvas
+    this.$shot = $shot
     this.init()
   }
   init () {
+    this.bindEvents()
+    this.startCamera()
+    this.context = this.$canvas.getContext('2d')
+  }
+  bindEvents () {
+    this.$shot.addEventListener('click', () => this.takePhoto())
+  }
+  startCamera () {
+    let localStream
     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || window.navigator.mozGetUserMedia
     window.URL = window.URL || window.webkitURL
-
-    let localStream
     navigator.getUserMedia({video: true, audio: false}, stream => {
       console.log(stream)
-      video.srcObject = stream
+      this.$video.srcObject = stream
     }, err => {
       console.error(err)
     })
   }
+  takePhoto () {
+    this.context.drawImage(this.$video, 0, 0, this.$canvas.width, this.$canvas.height)
+  }
 }
 
 new WebCamera({
-  $video: document.querySelector('#video')
+  $video: document.querySelector('.webcamera-video'),
+  $canvas: document.querySelector('.webcamera-canvas'),
+  $shot: document.querySelector('.webcamera-shot')
 })
